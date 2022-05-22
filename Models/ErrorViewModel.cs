2dfc6247606ad5e9abc5bbbe97b431dfbc8dc95c
Goncharov Dmitry <prototype1357@ukr.net>
2022-05-22 20:40:01 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crewing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
