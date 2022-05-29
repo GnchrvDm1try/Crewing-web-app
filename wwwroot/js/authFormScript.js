5a87d562b8c482fc1025a129c043105a25fb58bc
Goncharov Dmitry <prototype1357@ukr.net>
2022-05-29 14:21:53 +0300

    Added the js script for the authorization form.

---
+++ wwwroot/js/authFormScript.js
@@ -0,0 +1,30 @@
+﻿document.addEventListener('DOMcontentLoaded', setTimeout(function () { AuthFormPopup(true); }, 5000));
+
+function AuthFormPopup(isRegister) {
+    if (isRegister === true) {
+        document.getElementById('popupdiv').style.display = 'block'
+        document.getElementById('clientregdiv').style.display = 'block';
+    }
+    else {
+        document.getElementById('popupdiv').style.display = 'block'
+        document.getElementById('userlogindiv').style.display = 'block';
+    }
+}
+
+function AuthFormClose() {
+    document.getElementById('popupdiv').style.display = 'none';
+    document.getElementById('clientregdiv').style.display = 'none';
+    document.getElementById('employerregdiv').style.display = 'none';
+    document.getElementById('userlogindiv').style.display = 'none';
+}
+
+function AuthFormSwitch(isClient) {
+    if (isClient == true) {
+        document.getElementById('employerregdiv').style.display = 'none';
+        document.getElementById('clientregdiv').style.display = 'block';
+    }
+    else {
+        document.getElementById('clientregdiv').style.display = 'none';
+        document.getElementById('employerregdiv').style.display = 'block';
+    }
+}
