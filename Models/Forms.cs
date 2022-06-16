0dc4e330da41c47aa968049f858698574688d997
Goncharov Dmitry <prototype1357@ukr.net>
2022-06-16 20:47:32 +0300

    Created file "Forms.cs" with the ReviewCreationModel class, which represents the model for adding a new review.

---
+++ Models/Forms.cs
@@ -0,0 +1,9 @@
+﻿namespace Crewing.Models
+{
+    public class ReviewCreationModel
+    {
+        public string Comment { get; set; } = null!;
+        public double Estimation { get; set; }
+        public string Companyname { get; set; } = null!;
+    }
+}
