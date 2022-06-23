ec64f71a1f70ec70b944bdd4671af0cda510dc13
Goncharov Dmitry <prototype1357@ukr.net>
2022-06-23 22:57:13 +0300

    In the database was created view "Managerslastyearproductivity". After scaffolding its class appeared in the Models folder.

---
+++ Models/Managerslastyearproductivity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crewing.Models
+{
+    public partial class Managerslastyearproductivity
+    {
+        public int? Month { get; set; }
+        public int? Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Phonenumber { get; set; }
+        public string? Email { get; set; }
+        public long? Cc { get; set; }
+    }
+}
