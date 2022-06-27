a31310a42aefd6e252a5e04e6a85e68cb6433af1
Goncharov Dmitry <prototype1357@ukr.net>
2022-06-27 15:32:54 +0300

    ClientsVacancies model, which contains pairs of clients and vacancies has been created.

---
+++ Models/ClientsVacancies.cs
@@ -0,0 +1,9 @@
+﻿namespace Crewing.Models
+{
+    public class ClientsVacancies
+    {
+        public List<Client> Clients { get; set; } = null!;
+        public List<Vacancy> Vacancies { get; set; } = null!;
+        public Dictionary<int, int> IdPairs { get; set; } = new Dictionary<int, int>();
+    }
+}
