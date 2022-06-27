913f121cfdb122f4dca5b437d9ff34371be72a88
Goncharov Dmitry <prototype1357@ukr.net>
2022-06-27 15:18:10 +0300

    Created actions in the EmployeeController, which are needed for managing clients' requests.

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -50,5 +50,72 @@ namespace Crewing.Controllers
             var managersProductivity = await this.context.Managerslastyearproductivities.ToListAsync();
             return View(managersProductivity);
         }
+
+        // GET: Employee/ClientsAwaitingConfirmation
+        public async Task<IActionResult> ClientsAwaitingConfirmation()
+        {
+            var clients = await context.Clients
+                .Where(c => c.Status.StartsWith("Responded on vacancy with id "))
+                .ToListAsync();
+            ClientsVacancies dictionary = new ClientsVacancies();
+
+            foreach (var client in clients)
+            {
+                client.Status = String.Concat(client.Status.Where(Char.IsDigit));
+                if (!String.IsNullOrEmpty(client.Status))
+                {
+                    Vacancy? vacancy = await context.Vacancies
+                        .Where(v => v.Id == Convert.ToInt32(client.Status))
+                        .Include(v => v.Sailorpost)
+                        .FirstOrDefaultAsync();
+                    if (vacancy != null)
+                        dictionary.IdPairs.Add(client.Id, vacancy.Id);
+                }
+            }
+            dictionary.Clients = clients;
+            dictionary.Vacancies = await context.Vacancies.ToListAsync();
+            return View(dictionary);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ApproveClient(int clientId, int vacancyId)
+        {
+            Employee? employee = await context.Employees.Where(e => e.Email == User.Identity!.Name).FirstOrDefaultAsync();
+            Vacancy? vacancy = await context.Vacancies.FindAsync(vacancyId);
+            Client? client = await context.Clients.FindAsync(clientId);
+
+            if (client == null || vacancy == null || employee == null)
+                return NotFound();
+
+            client.Status = $"Working on vacancy with id {vacancyId}";
+            context.Update(client);
+            Contract contract = new Contract()
+            {
+                Clientid = clientId,
+                Employeeid = employee.Id,
+                Vacancyid = vacancy.Id
+            };
+            await context.Contracts.AddAsync(contract);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(ClientsAwaitingConfirmation));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RefuseClient(int clientId)
+        {
+            Client? client = await context.Clients.FindAsync(clientId);
+
+            if (client == null)
+                return NotFound();
+
+            client.Status = $"Looking for a job";
+            context.Update(client);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(ClientsAwaitingConfirmation));
+        }
     }
 }
